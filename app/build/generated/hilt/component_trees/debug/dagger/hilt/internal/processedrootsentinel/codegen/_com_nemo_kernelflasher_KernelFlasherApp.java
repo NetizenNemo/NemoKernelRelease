456164f003e884ec58b67ccc73fa246fbcf02ce8
NetizenNemo <shenzhenman20090818@outlook.com>
2026-06-21 14:47:55 +0800

    chore: fix .gitignore, remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_nemo_kernelflasher_KernelFlasherApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.nemo.kernelflasher.KernelFlasherApp"
-)
-public final class _com_nemo_kernelflasher_KernelFlasherApp {
-}
